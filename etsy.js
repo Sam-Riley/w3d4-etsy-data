55f97505928faf1e54febc2f64d6a93d3d6a3ddf
SAM RILEY <samriley1217@gmail.com>
2016-10-18 14:01:16 -0400

    finished yesterday

--- etsy.js
+++ etsy.js
@@ -1,27 +1,27 @@
-
+console.log(items)  //USE TO VERIFY YOU ARE PULLING IN CONTENT***
 
 //PROBLEM #1-CALCULATE THE AVERAGE PRICE OF ALL ITEMS
 
-// var total = items.reduce(function(previous, current){
-//     return {price: previous.price + current.price}
-// }, {price: 0})
-//
-// var avgPrice = total.price / items.length
-// console.log(total)
-//
-// console.log('The average price is ' + '$' + avgPrice)
+var total = items.reduce(function(previous, current) {
+    return {price: previous.price + current.price}
+}, {price: 0})
+
+var avgPrice = total.price / items.length
+console.log(total)
+
+console.log('The average price is ' + '$' + avgPrice)
 
 //I USED YOUR TEMPLATE FROM CLASS TO WRITE THIS CODE. I HAD TO CHANGE A FEW OF THE VARIABLES (WITH YOUR HELP KEK)
 
 //PROBLEM #2 -ARRAY OF ITEMS THAT COST BETWEEN $14.00 AND $18.00
 
-// var specificStuff = items.filter(function(listItems){
-//     if (listItems.price >= 14.00 && listItems.price <= 18.00) {
-//         return listItems
-//     }
-// })
-// console.log("Items that cost between $14.00 USD and $18.00 USD:")
-// console.log(specificStuff)
+var specificStuff = items.filter(function(listItems){
+    if (listItems.price >= 14.00 && listItems.price <= 18.00) {
+        return listItems
+    }
+})
+console.log("Items that cost between $14.00 USD and $18.00 USD:")
+console.log(specificStuff)
 
 // PLAYED AROUND WITH FILTER; I WAS HAVING FORMATTING ISSUES WITH THE ARRAY TRYING TO CONCATENATE THE STRING "ITEMS THAT COST....". KYLE SUGGESTED ANOTHER CONSOLE LOG TO PLACE IT ABOVE THE ARRAY.
 
@@ -36,28 +36,36 @@
 
 // PROBLEM #4 -FIND ITEMS MADE OF WOOD AND PRINT.
 
-var woodItems = items.filter(function(item){
-    return item.materials.includes('wood')
-})
+// var woodItems = items.filter(function(item){
+//     return item.materials.includes('wood')
+// })
 
 //---trying to play around with filter more and use it to pull in wood from materials
 
-// var woodItems = items.filter(function(item){
-//     return item.materials === "wood"
-// })
-console.log('All wood items:' + woodItems)
+
+// console.log('All wood items:' + woodItems)
 
 // NEED TO CHECK #4 AGAIN I AM TRYING TO USE FILTER AGAIN. TRIED TO ACCESS THE MATERIALS 2 DIFFERNT WAYS
 
 
 // PROBLEM #5 -FIND ITEMS WITH 8+ MATERIALS AND PRINT
 
-var materialsNumber = items.filter(function(item){
-    return item.materials.length >= 8
-})
-
-console.log(materialsNumber)
+// var materialsNumber = items.filter(function(item){
+//     return item.materials.length >= 8
+// })
+//
+// console.log(materialsNumber)
 
 // I USED FILTER AGAIN AND JUST ADDED A LENGTH REQUIREMENT TO THE MATERIALS SEGMENT
 
 // PROBLEM #6 -CALCULATE HOW MANY ITEMS
+
+var totalItems = items.reduce(function(total, currentItem) {
+    if (currentItem.whoMade === 'i_did') {
+        return total + 1
+    }
+    else {
+        return total
+    }
+}, 0)
+console.log(totalItems)
